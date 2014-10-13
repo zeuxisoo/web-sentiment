0d7303fe496c2c974d4fc1408305a4646ef9028a
Zeuxis Lo <seekstudio@gmail.com>
2014-10-13 23:22:48 +0800

    move advert into partial views, remove unwanted parts

--- app/views/homes/index.blade.php
+++ app/views/homes/index.blade.php
@@ -73,18 +73,9 @@
                     <a href="{{ url('/topic/create') }}" class="btn btn-info btn-lg">{{ trans('views.home.create_topic') }}</a>
                 </div>
             </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('views.home.random_users') }}</div>
-                <div class="panel-body row">
-                    @foreach($random_users as $user)
-                        <div class="col-md-4">
-                            <a href="#" class="thumb-random">
-                                <img src="{{ $user->avatar(64) }}" class="img-rounded" alt="{{{ $user->username }}}">
-                            </a>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
+
+            @include('partials.advert')
+
             <div class="panel panel-default">
                 <div class="panel-heading">{{ trans('views.home.advert') }}</div>
                 <div class="panel-body">
---
+++ app/views/partials/advert.blade.php
@@ -0,0 +1,12 @@
+<div class="panel panel-default">
+    <div class="panel-heading">{{ trans('views.home.random_users') }}</div>
+    <div class="panel-body row">
+        @foreach($random_users as $user)
+            <div class="col-md-4">
+                <a href="#" class="thumb-random">
+                    <img src="{{ $user->avatar(64) }}" class="img-rounded" alt="{{{ $user->username }}}">
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>

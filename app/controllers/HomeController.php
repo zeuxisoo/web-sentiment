b845bacb5a84c18175d3734f3feeca5c73e13ae5
Zeuxis Lo <seekstudio@gmail.com>
2014-10-16 17:52:43 +0800

    improve queries in HomeController@index

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,8 +2,8 @@
 class HomeController extends BaseController {
 
 	public function index() {
-        $latest_topics = Topic::with('User')->latest()->take(5)->get();
-        $hot_topics    = Topic::with('User')->hot()->take(5)->get();
+        $latest_topics = Topic::latest()->take(5)->with('user')->get();
+        $hot_topics    = Topic::hot()->take(5)->with('user')->get();
         $random_users  = User::random(6);
 
 		return View::make('homes.index', compact('latest_topics', 'hot_topics', 'random_users'));

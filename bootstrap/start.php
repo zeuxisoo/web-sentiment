0fb4220d5a91b14d3e6555543143cde49ee5a25c
Zeuxis Lo <seekstudio@gmail.com>
2014-10-24 16:28:08 +0800

    add new env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead', 'ZeuxisMBP.local'),
+	'local' => array('homestead', 'ZeuxisMBP.local', 'ZeuxisMmn.local'),
 
 ));
 

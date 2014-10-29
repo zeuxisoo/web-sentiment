86c5afd5553bee946b82944578351c2cfcc369b9
Zeuxis Lo <seekstudio@gmail.com>
2014-10-29 15:26:48 +0800

    rename migration file create_category to create_topic_creategory

--- app/database/migrations/2014_10_13_071205_create_topic_category_table.php
+++ app/database/migrations/2014_10_13_071205_create_topic_category_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoryTable extends Migration {
+class CreateTopicCategoryTable extends Migration {
 
 	/**
 	 * Run the migrations.

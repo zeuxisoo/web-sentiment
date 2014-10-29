9b58c8bac5e506db006d2dd3872bd1a0c486e742
Zeuxis Lo <seekstudio@gmail.com>
2014-10-29 14:44:21 +0800

    fix advert block image y-overflow

--- app/views/blocks/advert.blade.php
+++ app/views/blocks/advert.blade.php
@@ -1,6 +1,8 @@
 <div class="panel panel-default">
     <div class="panel-heading">{{ trans('views.home.advert') }}</div>
     <div class="panel-body">
-        <img src="http://placehold.it/225x225">
+        <a class="thumbnail thumbnail-no-border">
+            <img src="http://placehold.it/225x225">
+        </a>
     </div>
 </div>

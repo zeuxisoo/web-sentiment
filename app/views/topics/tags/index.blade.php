a6e9a1d67779fd882fc38de5712327deef977d8c
Zeuxis Lo <seekstudio@gmail.com>
2014-11-10 14:49:08 +0800

    change the related topics will not display by default when tag was not selected

--- app/views/topics/tags/index.blade.php
+++ app/views/topics/tags/index.blade.php
@@ -3,21 +3,23 @@
 @section('container')
     <div class="topic-category">
         <div class="container">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('views.topic_tags.heading', ['slug' => e($slug)]) }}</div>
-                <div class="panel-body">
-                    @if ($topics->isEmpty())
-                        <small class="text-muted">{{ trans('views.topic_tags.no_related_topics') }}</small>
-                    @else
-                        @foreach($topics as $topic)
-                            @include('homes.partials.topic')
-                        @endforeach
-                    @endif
+            @if (empty($slug) === false)
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{ trans('views.topic_tags.heading', ['slug' => e($slug)]) }}</div>
+                    <div class="panel-body">
+                        @if ($topics->isEmpty())
+                            <small class="text-muted">{{ trans('views.topic_tags.no_related_topics') }}</small>
+                        @else
+                            @foreach($topics as $topic)
+                                @include('homes.partials.topic')
+                            @endforeach
+                        @endif
+                    </div>
+                    <div class="panel-footer">
+                        {{ $topics->links('paginations.back_next'); }}
+                    </div>
                 </div>
-                <div class="panel-footer">
-                    {{ $topics->links('paginations.back_next'); }}
-                </div>
-            </div>
+            @endif
 
             <div class="panel panel-default">
                 <div class="panel-heading">{{ trans('views.topic_tags.all_tags') }}</div>

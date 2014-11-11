70356b9a0a2afff309e67c102279b2a7c67879cf
Zeuxis Lo <seekstudio@gmail.com>
2014-11-11 14:50:14 +0800

    make advert placeholder using https

--- app/views/blocks/advert.blade.php
+++ app/views/blocks/advert.blade.php
@@ -2,7 +2,7 @@
     <div class="panel-heading">{{ trans('views.home.advert') }}</div>
     <div class="panel-body">
         <a class="thumbnail thumbnail-no-border">
-            <img src="http://placehold.it/225x225">
+            <img src="https://placehold.it/225x225">
         </a>
     </div>
 </div>

92a2c08928d0a4950d0447b9db2e237f15594f43
Zeuxis Lo <seekstudio@gmail.com>
2014-11-11 14:37:20 +0800

    move social sign in button text into i18n file

--- app/views/auths/partials/social_login.blade.php
+++ app/views/auths/partials/social_login.blade.php
@@ -3,7 +3,7 @@
     <div class="panel-body row">
         <div class="col-md-3">
             <a href="{{ route('oauth.connect.facebook') }}" class="btn btn-block btn-social btn-facebook">
-                <i class="fa fa-facebook"></i>&nbsp;Sign in with Twitter
+                <i class="fa fa-facebook"></i>&nbsp;{{ trans('views.auth.signin_with_facebook') }}
             </a>
         </div>
     </div>

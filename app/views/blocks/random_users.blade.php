93a13095bc6965303867eddc3d2b352fdceee7c6
Zeuxis Lo <seekstudio@gmail.com>
2014-11-12 15:52:33 +0800

    fix random user block in responsive layout will overflow

--- app/views/blocks/random_users.blade.php
+++ app/views/blocks/random_users.blade.php
@@ -2,7 +2,7 @@
     <div class="panel-heading">{{ trans('views.home.random_users') }}</div>
     <div class="panel-body row">
         @foreach($random_users as $user)
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6 col-sm-3 col-xs-4">
                 <a href="{{ route('user.profile', ['username' => $user->username]) }}" class="thumb-random">
                     <img src="{{ $user->avatar(64) }}" class="img-rounded" alt="{{{ $user->username }}}">
                 </a>

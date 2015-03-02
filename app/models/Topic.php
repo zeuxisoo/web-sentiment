c728de6d757a8974e7a4e5245dfcff45563089b3
Zeuxis Lo <seekstudio@gmail.com>
2015-03-02 13:42:35 +0800

    remove cover field in fillable list in topic model

--- app/models/Topic.php
+++ app/models/Topic.php
@@ -14,7 +14,7 @@ class Topic extends Eloquent {
     protected $table = 'topic';
 
     protected $fillable = [
-        'user_id', 'topic_category_id', 'subject', 'cover', 'description',
+        'user_id', 'topic_category_id', 'subject', 'description',
         'answer_a_text', 'answer_b_text', 'answer_a_image', 'answer_b_image'
     ];
 
